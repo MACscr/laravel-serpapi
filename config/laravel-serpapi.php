620518c56f35285ad393715c66c39c7f94a64019
Mark Chaney <macscr@macscr.com>
2021-12-30 21:26:51 -0500

    now working

--- config/laravel-serpapi.php
+++ config/laravel-serpapi.php
@@ -2,5 +2,5 @@
 
 return [
     'api_key' => env('SERPAPI_API_KEY'),
-    'search_engine' => env('SERPAPI_ENGINE', 'google')
+    'search_engine' => env('SERPAPI_ENGINE', 'google'),
 ];
--- src/LaravelSerpapiServiceProvider.php
+++ src/LaravelSerpapiServiceProvider.php
@@ -7,11 +7,24 @@ namespace Macscr\LaravelSerpapi;
 use Illuminate\Support\ServiceProvider;
 use Macscr\LaravelSerpapi\Helpers\SerpApiSearch;
 
-class LaravelSerpAPIServiceProvider extends ServiceProvider
+class LaravelSerpapiServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap any package services.
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/laravel-serpapi.php' => config_path('laravel-serpapi.php'),
+        ]);
+    }
+
     public function register()
     {
-        parent::register();
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/laravel-serpapi.php',
+            'laravel-serpapi'
+        );
 
         $this->app->bind(SerpApiSearch::class, function () {
             $api_key = config('laravel-serpapi.api_key');
